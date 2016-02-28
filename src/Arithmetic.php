9c2fa37cac5f82057e59047ab6a9ffd1dd61a695
tomsato <tmhr.st9711@gmail.com>
2016-02-29 04:32:28 +0900

    doxygen test

--- src/Arithmetic.php
+++ src/Arithmetic.php
@@ -5,12 +5,12 @@
 
 class Arithmetic
 {
-    /*
-     * @brief 足し算を行う関数
-     * @param x 項1
-     * @param y 項2
-     * @return xとyを足した結果
-     */
+    /**
+     ** @brief 足し算を行う関数
+     ** @param x 項1
+     ** @param y 項2
+     ** @return xとyを足した結果
+     **/
     public function add($x, $y) {
         return($x + $y);
     }
